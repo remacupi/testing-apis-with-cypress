b3cb62c387784ee5ca0aacce988a2e24db3ba92d
ReyesMCuesta <reyesm.cuesta@cesurformacion.com>
2022-05-20 15:42:18 +0200

    update gitignore
    update gitignore

--- cypress/integration/api-tests/api-objects/clean_entities_fixtures.js
+++ cypress/integration/api-tests/api-objects/clean_entities_fixtures.js
@@ -1,6 +1,7 @@
 import { DeleteEntitiesApi } from "../api-objects/delete_entities_api"
 
 //remove created entity after each scenario
+//the recomendation is to clean before run the test, but the api does not allow to delete by name or custom tag, so I have to delete after
 afterEach(() => {
   const deleteEntitiesApi = new DeleteEntitiesApi();
   deleteEntitiesApi.deleteCreatedEntity();
